abb19a3c8217e6b932a4d4d27235e830d59390b7
Shahriar-0 <attarshahriar@gmail.com>
2023-10-09 17:30:13 +0330

    format

--- Archive - Spring 1402 - EX 1/Related Codes/mmm.c
+++ Archive - Spring 1402 - EX 1/Related Codes/mmm.c
@@ -1,24 +1,22 @@
+#include "fcntl.h"
 #include "types.h"
 #include "user.h"
-#include "fcntl.h"
 
-int
-custom_atoi(const char *str)
-{
+int custom_atoi(const char* str) {
     int sign = 1;
     int unsigned_num = 0;
 
-    if(*str == '-'){
+    if (*str == '-') {
         sign = -1;
         ++str;
     }
-    else if(*str == '+')
+    else if (*str == '+')
         ++str;
 
-    for(int i = 0; str[i] != '\0'; i++){
-        if(str[i] >= '0' && str[i] <= '9')
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= '0' && str[i] <= '9')
             unsigned_num = unsigned_num * 10 + (str[i] - '0');
-        else{
+        else {
             printf(2, "Error: Wrong number format.\n");
             exit();
         }
@@ -26,18 +24,16 @@ custom_atoi(const char *str)
     return sign * unsigned_num;
 }
 
-void
-sort(int *numbers, int len)
-{
+void sort(int* numbers, int len) {
     int i;
     int j;
     int key = 0;
 
-    for(i = 1; i < len; i++){
+    for (i = 1; i < len; i++) {
         key = numbers[i];
         j = i - 1;
 
-        while(j >= 0 && numbers[j] > key){
+        while (j >= 0 && numbers[j] > key) {
             numbers[j + 1] = numbers[j];
             j = j - 1;
         }
@@ -45,46 +41,40 @@ sort(int *numbers, int len)
     }
 }
 
-int
-mean(int *numbers, int len)
-{
+int mean(int* numbers, int len) {
     int sum = 0;
-    for(int i = 0; i < len; i++)
+    for (int i = 0; i < len; i++)
         sum += numbers[i];
     return (int)(sum / len);
 }
 
-int
-median(int *numbers, int len)
-{
+int median(int* numbers, int len) {
     int median;
     int idx;
 
-    if(len % 2 == 0){
+    if (len % 2 == 0) {
         idx = len / 2;
         median = (int)((numbers[idx - 1] + numbers[idx]) / 2);
     }
-    else{
+    else {
         idx = (int)(len / 2);
         median = numbers[idx];
     }
     return median;
 }
 
-int
-mode(int *numbers, int len)
-{
+int mode(int* numbers, int len) {
     int mode = numbers[0];
     int max_occurence = 0;
 
-    for(int i = 0; i < len; i++){
+    for (int i = 0; i < len; i++) {
         int occurrence_count = 0;
 
         for (int j = 0; j < len; j++)
             if (numbers[j] == numbers[i])
-                occurrence_count++; 
+                occurrence_count++;
 
-        if(occurrence_count > max_occurence){
+        if (occurrence_count > max_occurence) {
             max_occurence = occurrence_count;
             mode = numbers[i];
         }
@@ -92,26 +82,25 @@ mode(int *numbers, int len)
     return mode;
 }
 
-int main(int argc, char *argv[])
-{
-    if(argc < 2){
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
         printf(2, "usage: mmm <array_of_numbers>\n");
         exit();
     }
-    else if(argc > 8){
+    else if (argc > 8) {
         printf(2, "Error: Please ensure that you enter no more than 7 numbers.\n");
         exit();
     }
 
     int numbers[7];
     int len = argc - 1;
-    for(int i = 0; i < len; i++)
+    for (int i = 0; i < len; i++)
         numbers[i] = custom_atoi(argv[i + 1]);
     sort(numbers, len);
 
     unlink("mmm_result.txt");
     int fd = open("mmm_result.txt", O_CREATE | O_WRONLY);
-    if(fd < 0){
+    if (fd < 0) {
         printf(2, "mmm: cannot create mmm_result.txt\n");
         exit();
     }
