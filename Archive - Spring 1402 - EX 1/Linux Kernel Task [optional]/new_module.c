7dbc116361ce9e354956504d2337bbd46f5fdb82
Shahriar-0 <attarshahriar@gmail.com>
2023-10-09 18:27:01 +0330

    fix new module

--- Archive - Spring 1402 - EX 1/Linux Kernel Task [optional]/new_module.c
+++ Archive - Spring 1402 - EX 1/Linux Kernel Task [optional]/new_module.c
@@ -1,10 +1,9 @@
-#include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
 
 MODULE_LICENSE("GPL");
 
-int init_module(void)
-{
+int init_module(void) {
     printk(KERN_INFO "Group Members:\n    - Ali Ghanbari\n    - Behrad Elmi\n    - Bita Nasiri\n");
     return 0;
 }
