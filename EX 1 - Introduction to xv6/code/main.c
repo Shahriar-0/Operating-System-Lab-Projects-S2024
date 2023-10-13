8b9ab88c1a33632dabb1754d8daf1121ac2c112e
Shahriar-0 <attarshahriar@gmail.com>
2023-10-13 14:28:44 +0330

    add intro and update makefile

--- EX 1 - Introduction to xv6/code/main.c
+++ EX 1 - Introduction to xv6/code/main.c
@@ -1,5 +1,19 @@
-#include <stdio.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
 
-int main(int argc, char* argv[]) {
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Group's members");
+MODULE_DESCRIPTION("this module print group's members name");
+
+static int __init print_members_name(void) {
+    printk(KERN_INFO "Group 1:\n- Matin Bazrafshan : 810100***\n- Sobhan Alaedini      : 810100***\n- Shahriar Attar    : 810100186\n Welcome :)\n");
     return 0;
 }
+
+static void __exit outro(void) {
+    printk(KERN_INFO "Goodbye :)\n");
+}
+
+module_init(print_members_name);
+module_exit(outro);
