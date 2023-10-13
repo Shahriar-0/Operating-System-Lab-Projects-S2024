1e03f1f84494989b5ad8d899f7d93a8af374a031
Shahriar-0 <attarshahriar@gmail.com>
2023-10-13 14:34:16 +0330

    fix align

--- EX 1 - Introduction to xv6/code/main.c
+++ EX 1 - Introduction to xv6/code/main.c
@@ -1,13 +1,13 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Group's members");
 MODULE_DESCRIPTION("this module print group's members name");
 
 static int __init print_members_name(void) {
-    printk(KERN_INFO "Group 1:\n- Matin Bazrafshan : 810100***\n- Sobhan Alaedini      : 810100***\n- Shahriar Attar    : 810100186\n Welcome :)\n");
+    printk(KERN_INFO "Group 1:\n- Matin Bazrafshan : 810100***\n- Sobhan Alaedini  : 810100***\n- Shahriar Attar   : 810100186\n Welcome :)\n");
     return 0;
 }
 
